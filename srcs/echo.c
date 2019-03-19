794d0af3b5d0c3f1ac9da3a15ae63bc63fa233f6
Thomas LANDEMAINE <tlandema@e2r13p2.42.fr>
2019-03-19 16:40:43 +0100

    The echo builtin is nice, there is no more leaks while using it

--- srcs/echo.c
+++ srcs/echo.c
@@ -6,7 +6,7 @@
 /*   By: tlandema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 11:39:10 by tlandema          #+#    #+#             */
-/*   Updated: 2019/03/19 09:34:24 by tlandema         ###   ########.fr       */
+/*   Updated: 2019/03/19 16:39:12 by tlandema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,17 @@
 
 static int	ft_echo_helper_2(t_env *envir, char *str, int i)
 {
-	char *tmp;
+	char	*tmp;
+	char	*to_f;
 
 	tmp = ft_strsub(&str[i], 0, ft_stristr(&str[i], " "));
-	if (tmp[ft_strlen(tmp)] == '"')
-		ft_echo_dolss(envir, ft_strsub(tmp, 0, ft_strlen(tmp) - 1));
+	to_f = NULL;
+	if (tmp[ft_strlen(tmp) - 1] == '"')
+		ft_echo_dolss(envir, (to_f = ft_strsub(tmp, 0, ft_strlen(tmp) - 1)));
 	else
 		ft_echo_dolss(envir, tmp);
-	ft_putstr(tmp);
 	free(tmp);
+	free(to_f);
 	i += ft_stristr(&str[i], " ");
 	return (i);
 }
@@ -58,6 +60,7 @@ void	ft_echo_helper(t_env *envir, char **tab)
 		else if (str[i] != '"')
 			ft_putchar(str[i]);
 	}
+	free(str);
 }
 
 void	ft_echo_dolss(t_env *envir, char *str)
@@ -65,6 +68,7 @@ void	ft_echo_dolss(t_env *envir, char *str)
 	int		i;
 	char	*str_tmp;
 	char	*str_tmp2;
+	char	*str_tmp3;
 	char	**tmp;
 
 	i = 0;
@@ -74,9 +78,12 @@ void	ft_echo_dolss(t_env *envir, char *str)
 		str_tmp = ft_strdup(tmp[i]);
 		str_tmp = ft_strrev(str_tmp);
 		str_tmp2 = ft_strdup(str_tmp);
-		str_tmp2 = ft_strrev(&ft_strchr(str_tmp, '=')[1]);
-		if (ft_strequ(&str[1], str_tmp2))
+		str_tmp3 = ft_strrev(&ft_strchr(str_tmp, '=')[1]);
+		if (ft_strequ(&str[1], str_tmp3))
 			ft_putstr(&ft_strchr(ft_strrev(str_tmp), '=')[1]);
 		i++;
+		free(str_tmp);
+		free(str_tmp2);
 	}
+	ft_tabdel(ft_count_tab(tmp), &tmp);
 }
